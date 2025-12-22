0a2f4a0348677305c5f0709ca9239db57994a77c
Daniel Kusai <danielk@copyleaks.com>
2025-12-22 15:51:29 +0200

    quickstart: added comment for WSL

--- quickstart.sh
+++ quickstart.sh
@@ -3,3 +3,6 @@ sudo apt-get install python3-pip luarocks ripgrep fd-find python3-venv
 
 curl https://sh.rustup.rs -sSf | sh
 cargo install --locked tree-sitter-cli
+
+# For WSL
+# sudo apt install xclip
