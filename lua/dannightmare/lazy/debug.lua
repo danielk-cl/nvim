c58673ecfb356145caeeb2822192551aa9d2a4d6
Daniel Kusai <danielk@copyleaks.com>
2025-12-22 15:27:53 +0200

    WIP debugging

---
+++ lua/dannightmare/lazy/debug.lua
@@ -0,0 +1,29 @@
+return {
+    {
+        "HiPhish/debugpy.nvim",
+        dependencies = { "mfussenegger/nvim-dap", }
+    },
+    {
+        "rcarriga/nvim-dap-ui",
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" }
+    },
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-neotest/nvim-nio",
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-neotest/neotest-python",
+        },
+        config = function()
+            require("neotest").setup({
+                adapters = {
+                    require("neotest-python")({
+                        runner = "python-unittest",
+                    })
+                }
+            })
+        end
+    }
+}
