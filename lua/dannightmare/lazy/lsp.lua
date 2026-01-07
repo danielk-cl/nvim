a8fe0ca958fe179fdccea17e09afed02b5f0e9a4
Daniel Kusai <danielk@copyleaks.com>
2026-01-07 18:40:03 +0200

    Disabled basedpyright's lsp providing
    Also fixed a bug from the previous commits

--- lua/dannightmare/lazy/lsp.lua
+++ lua/dannightmare/lazy/lsp.lua
@@ -2,10 +2,67 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = {
         {
-            "mason-org/mason.nvim",
-            opts = {}
+            dependencies = {
+                "mason-org/mason.nvim",
+                opts = {}
+            },
+            "mason-org/mason-lspconfig.nvim",
+            opts = {
+                ensure_installed = {
+                    "lua_ls",        -- lua
+                    "rust_analyzer", -- rust
+                    -- "gopls",         -- golang
+                    -- python
+                    "basedpyright",
+                    "pylsp",
+                    "clangd", -- C, C++, and more
+                },
+                -- handlers = {
+                --     function(server_name) -- default handler (optional)
+                --         require("lspconfig")[server_name].setup {
+                --             capabilities = capabilities
+                --         }
+                --     end,
+                --
+                --     ["clangd"] = function()
+                --         local lspconfig = require("lspconfig")
+                --         lspconfig.clangd.setup {
+                --             capabilities = capabilities,
+                --             cmd = { "clangd", "--background-index", "--clang-tidy" },
+                --             filetypes = { "c", "cpp", "h", "hpp" },
+                --             root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt"),
+                --         }
+                --     end,
+                --
+                --     ["lua_ls"] = function()
+                --         local lspconfig = require("lspconfig")
+                --         lspconfig.lua_ls.setup {
+                --             capabilities = capabilities,
+                --             settings = {
+                --                 Lua = {
+                --                     runtime = { version = "Lua 5.1" },
+                --                     diagnostics = {
+                --                         globals = { "vim", "it", "describe", "before_each", "after_each" },
+                --                     }
+                --                 }
+                --             }
+                --         }
+                --     end,
+                --
+                --     ["basedpyright"] = function()
+                --         local lspconfig = require("lspconfig")
+                --
+                --         lspconfig.basedpyright.setup {
+                --             capabilities = capabilities,
+                --             on_attach = function(client, bufnr)
+                --                 client.server_capabilities.definitionProvider = false
+                --                 client.server_capabilities.referencesProvider = false
+                --             end
+                --         }
+                --     end
+                -- }
+            },
         },
-        "williamboman/mason-lspconfig.nvim",
         "L3MON4D3/LuaSnip",
         {
             "j-hui/fidget.nvim",
@@ -30,63 +87,8 @@ return {
 
     },
 
-    opts = {
-            ensure_installed = {
-                "lua_ls",        -- lua
-                "rust_analyzer", -- rust
-                -- "gopls",         -- golang
-                -- python
-                "basedpyright",
-                "pylsp",
-                "clangd", -- C, C++, and more
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
-
-                ["clangd"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.clangd.setup {
-                        capabilities = capabilities,
-                        cmd = { "clangd", "--background-index", "--clang-tidy" },
-                        filetypes = { "c", "cpp", "h", "hpp" },
-                        root_dir = lspconfig.util.root_pattern("compile_commands.json", "compile_flags.txt"),
-                    }
-                end,
-
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-
-                ["basedpyright"] = function()
-                    local lspconfig = require("lspconfig")
-
-                    lspconfig.basedpyright.setup {
-                        capabilities = capabilities,
-                        on_attach = function(client, bufnr)
-                            client.server_capabilities.definitionProvider = false
-                            client.server_capabilities.referencesProvider = false
-                        end
-                    }
-                end
-            }
-        },
-
     config = function()
+        vim.lsp.config('basedpyright', { settings = { basedpyright = { disableLanguageServices = true } } } )
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
             "force",
